30a6b2622795be6b00cdbd971e6e5fe89d6e2992
Vinicius Fidelis <vinicius.curso.tecnico.senai@gmail.com>
2023-04-06 21:34:29 -0300

    salvando

---
+++ variaveiseapresentacoesemtela.js
@@ -0,0 +1,5 @@
+var nome = prompt("Qual é o seu nome?")
+var sobrenome = prompt("Qual é o seu sobrenome?")
+
+document.write("<h1>Bem vindo "+nome+" "+sobrenome+"</h1>")
+document.write("<img src=img/Elon_Musk_.jpg />")
